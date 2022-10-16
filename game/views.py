7b56012be879c5ad8adb5fcabb96fd898edc28a9
= <=>
2022-10-16 15:33:13 +0800

    my page

--- game/views.py
+++ game/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
 def index (request):
-    return HttpResponse("伟大的史诗级巨著")
+    line1 = '<h1 style="text-align: center">术士之战</h1>'
+    return HttpResponse(line1)
+
+def play (request):
+    line1 = ' <h1 style="text-align: center">游戏界面</h1>'
+    return HttpResponse(line1)
